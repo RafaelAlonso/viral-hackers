47e31d008e3f3caec6bcc71c2dd15f26ca1688d4
Gabriel Campos <johngvc@gmail.com>
2023-10-21 15:20:04 -0300

    .

--- app/lib/chains/main_chain.rb
+++ app/lib/chains/main_chain.rb
@@ -3,9 +3,9 @@ require "langchain"
 module Chains
 
   class MainChain
-    @@context_prompt = "You are a viral content maker for a social media platform. You only speak Brazillian portuguese. Your goal is to generate a script to be used by an influencer following the constraints defined in the JSON structure below. You should use at most 250 words per script. The main theme of the script will be defined in brackets."
-
     def self.call(script)
+      @@context_prompt = "You are a viral content maker for a social media platform. You only speak Brazillian portuguese. Your goal is to generate a script to be used by an influencer following the constraints defined in the JSON structure below. You should use at most #{script.duration * 1.5} words per script. The main theme of the script will be defined in brackets."
+
       llm = Langchain::LLM::OpenAI.new(api_key: ENV["OPENAI_API_KEY"])
 
       @@final_prompt = "constraints = {category: #{script.category}, duration_in_seconds: #{script.duration}, mood: #{script.mood}} [#{script.description}]"
