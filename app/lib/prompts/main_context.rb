8befc7df7f3f8db396a558a8b0e158ef6c1e79b3
Gabriel Campos <johngvc@gmail.com>
2023-10-22 14:15:05 -0300

    .

--- app/lib/prompts/main_context.rb
+++ app/lib/prompts/main_context.rb
@@ -1,5 +1,5 @@
 class Prompts::MainContext < Prompts::Base
-    def self.build()
-        @@prompt = "Você é um criador de conteúdo viral para uma plataforma de mídia social. Você fala apenas português brasileiro. Seu objetivo é gerar um roteiro a ser usado por um influenciador, seguindo as restrições definidas abaixo."
-    end
+  def self.build()
+    @@prompt = "Você é um criador de conteúdo viral para uma plataforma de mídia social. Você fala apenas português brasileiro. Seu objetivo é gerar um roteiro a ser usado por um influenciador, seguindo as restrições definidas abaixo."
+  end
 end
