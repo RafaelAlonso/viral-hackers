850aec76a28b39e467cf96e1825191dc46112882
johngvc <42398896+johngvc@users.noreply.github.com>
2023-10-22 14:15:44 -0300

    Implement long video chain (#14)
    .

---
+++ app/lib/chains/long_video_chain.rb
@@ -0,0 +1,20 @@
+module Chains
+  class LongVideoChain
+    def self.call(script)
+      @@base_script = script.output
+
+      @@context_prompt = "#{Prompts::MainContext.build()} #{Prompts::LongVideoDefinitionConstraints.build(@@base_script)}"
+
+      llm = Langchain::LLM::OpenAI.new(api_key: ENV["OPENAI_API_KEY"])
+
+      @@message_prompt = "restrições = {categoria: #{script.category}, tom_do_roteiro: #{script.mood}, audiência_alvo: #{script.target_audience}
+      
+      Conteúdo detalhado e completo com mais de 10 parágrafos:
+      "
+
+      chat = Langchain::Conversation.new(llm: llm, temperature: 0.9, model: "gpt-4")
+      chat.set_context(@@context_prompt)
+      chat.message(@@message_prompt)
+    end
+  end
+end
